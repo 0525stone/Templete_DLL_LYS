501338aa34a59a4afb7ab2d47793ebb5a1ee8c4e
Yunsuk Lee <0525stone@gmail.com>
2023-11-01 11:11:12 +0900

    리팩토링 Draw mode 완성
    TODO
    - DLL 연결 예정

--- Templete_DLL_LYS/Form1.cs
+++ Templete_DLL_LYS/Form1.cs
@@ -148,7 +148,8 @@ namespace Templete_DLL_LYS
                 {
                     g = MainPalette.CreateGraphics(); // Graphics 객체 생성(원)
                     g.DrawLine(pen, m_startPoint, m_endPoint);
-                    m_lines.Add(new List<Point> { m_startPoint, m_endPoint });
+                    m_points.Add(m_startPoint);
+                    m_points.Add(m_endPoint);
                     logs_drawing.Add(2);
                 }
             }
@@ -156,33 +157,31 @@ namespace Templete_DLL_LYS
 
         private void MainPalette_KeyDown(object sender, KeyEventArgs e)
         {
-            string keypressed = "Key Pressed : " + m_points.Count.ToString();
-            Console.WriteLine(keypressed);
             if (m_ProgramMode == 0)
             {
-                if (e.KeyCode == Keys.D)
+                if ((e.KeyCode == Keys.D) && (logs_drawing.Count > 0))
                 {
+                    int log_current = logs_drawing[logs_drawing.Count - 1];
 
-                    int a = logs_drawing[logs_drawing.Count - 1];
-                    if (a == 1)
+                    if (log_current == 1)
                     {
                         if (m_points.Count > 0)
                         {
                             m_points.RemoveAt(m_points.Count - 1);
                             logs_drawing.RemoveAt(logs_drawing.Count - 1);
-                            MainPalette.Invalidate(); // Request the panel to be repainted
                         }
                     }
-                    if (a==2)
+                    if (log_current == 2)
                     {
-                        if (m_lines.Count > 0)
+                        if (m_points.Count > 0)
                         {
-                            m_lines.RemoveAt(m_lines.Count - 1);
+                            m_points.RemoveAt(m_points.Count - 1);
+                            m_points.RemoveAt(m_points.Count - 1);
                             logs_drawing.RemoveAt(logs_drawing.Count - 1);
-                            MainPalette.Invalidate();
                         }
 
                     }
+                    MainPalette.Invalidate(); // Request the panel to be repainted
                     MainPallete_repaint();
                 }
             }
@@ -195,13 +194,19 @@ namespace Templete_DLL_LYS
             using (Graphics g = Graphics.FromImage(bitmap))
             {
                 g.Clear(Color.White);
-                foreach (Point point in m_points)
+                int index_point = 0;
+                foreach (int index_logs in logs_drawing)
                 {
-                    g.DrawEllipse(pen, point.X, point.Y, 2 * radius, 2 * radius);
-                }
-                foreach (List<Point> line in m_lines)
-                {
-                    g.DrawLine(pen, line[0], line[1]);
+                    if (index_logs == 1)
+                    {
+                        g.DrawEllipse(pen, m_points[index_point].X, m_points[index_point].Y, 2 * radius, 2 * radius);
+                        index_point += index_logs;
+                    }
+                    else if (index_logs == 2)
+                    {
+                        g.DrawLine(pen, m_points[index_point], m_points[index_point+1]);
+                        index_point += index_logs;
+                    }
                 }
 
             }
